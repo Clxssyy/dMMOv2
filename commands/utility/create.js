45b4b3d795772355e64d2c9da18566853668dcdc
Michael Connolly <mpc63@uakron.edu>
2023-11-26 02:13:07 -0500

    Add zone / region creation logging

--- commands/utility/create.js
+++ commands/utility/create.js
@@ -2,6 +2,8 @@ const {
   SlashCommandBuilder,
   ChannelType,
   PermissionsBitField,
+  EmbedBuilder,
+  AttachmentBuilder,
 } = require('discord.js');
 
 module.exports = {
@@ -46,10 +48,17 @@ module.exports = {
   async execute(interaction) {
     await interaction.deferReply({ ephemeral: true });
 
+    const file = await new AttachmentBuilder(
+      './public/tick-check-mark-icon-sign-symbol-design-free-png.webp'
+    );
+
     if (interaction.options.getSubcommand() === 'zone') {
       const name = interaction.options.getString('name');
       const region = interaction.options.getChannel('region');
       const description = interaction.options.getString('description');
+
+      let zone;
+      let zoneRole;
 
       await interaction.followUp({
         content: `Creating zone ${name} in ${region.name}${
@@ -57,10 +66,14 @@ module.exports = {
         }`,
       });
 
-      await interaction.guild.roles.create({
-        name: name,
-        mentionable: true,
-      });
+      await interaction.guild.roles
+        .create({
+          name: name,
+          mentionable: true,
+        })
+        .then((role) => {
+          zoneRole = role;
+        });
 
       await interaction.guild.channels
         .create({
@@ -73,9 +86,7 @@ module.exports = {
               deny: [PermissionsBitField.Flags.ViewChannel],
             },
             {
-              id: interaction.guild.roles.cache.find(
-                (role) => role.name === name
-              ),
+              id: zoneRole,
               allow: [
                 PermissionsBitField.Flags.ViewChannel,
                 PermissionsBitField.Flags.SendMessages,
@@ -86,6 +97,7 @@ module.exports = {
         })
         .then((channel) => {
           channel.setParent(region);
+          zone = channel;
         });
 
       await interaction.editReply({
@@ -93,6 +105,53 @@ module.exports = {
           description ? ' with description ' + description : ''
         }`,
       });
+
+      const zoneEmbed = await new EmbedBuilder()
+        .setTitle('Zone Creation')
+        .setThumbnail(
+          'attachment://tick-check-mark-icon-sign-symbol-design-free-png.webp'
+        )
+        .setColor(0x00ff7d)
+        .addFields([
+          {
+            name: 'Name',
+            value: `<#${zone.id}>`,
+            inline: true,
+          },
+          {
+            name: 'Region',
+            value: region.id,
+            inline: true,
+          },
+          {
+            name: 'Description',
+            value: description ? description : 'No description given.',
+            inline: false,
+          },
+          {
+            name: 'Role',
+            value: `<@&${zoneRole.id}>`,
+            inline: false,
+          },
+          {
+            name: 'By',
+            value: `<@${interaction.user.id}>`,
+            inline: true,
+          },
+          {
+            name: '\u200b',
+            value: `<t:${Math.floor(Date.now() / 1000)}:R>`,
+            inline: true,
+          },
+        ])
+        .setTimestamp();
+
+      await interaction.guild.channels.cache
+        .find((channel) => channel.id === '1178199804753481789')
+        .send({
+          embeds: [zoneEmbed],
+          files: [file],
+        });
     }
 
     if (interaction.options.getSubcommand() === 'region') {
@@ -101,44 +160,88 @@ module.exports = {
         (channel) => channel.id === '1178211185640869968'
       );
 
-      console.log('playerCategory', playerCategory.position);
       const position = playerCategory.position;
-      console.log(position);
+
+      let region;
+      let regionRole;
 
       await interaction.followUp({
         content: `Creating region ${name}.`,
       });
 
-      await interaction.guild.roles.create({
-        name: name,
-        mentionable: true,
-      });
+      await interaction.guild.roles
+        .create({
+          name: name,
+          mentionable: true,
+        })
+        .then((role) => {
+          regionRole = role;
+        });
 
-      await interaction.guild.channels.create({
-        name: name,
-        type: ChannelType.GuildCategory,
-        position: position,
-        permissionOverwrites: [
-          {
-            id: interaction.guild.id,
-            deny: [PermissionsBitField.Flags.ViewChannel],
-          },
-          {
-            id: interaction.guild.roles.cache.find(
-              (role) => role.name === name
-            ),
-            allow: [
-              PermissionsBitField.Flags.ViewChannel,
-              PermissionsBitField.Flags.SendMessages,
-              PermissionsBitField.Flags.ReadMessageHistory,
-            ],
-          },
-        ],
-      });
+      await interaction.guild.channels
+        .create({
+          name: name,
+          type: ChannelType.GuildCategory,
+          position: position,
+          permissionOverwrites: [
+            {
+              id: interaction.guild.id,
+              deny: [PermissionsBitField.Flags.ViewChannel],
+            },
+            {
+              id: regionRole,
+              allow: [
+                PermissionsBitField.Flags.ViewChannel,
+                PermissionsBitField.Flags.SendMessages,
+                PermissionsBitField.Flags.ReadMessageHistory,
+              ],
+            },
+          ],
+        })
+        .then((channel) => {
+          region = channel;
+        });
 
       await interaction.editReply({
         content: `Created region ${name}.`,
       });
+
+      const regionEmbed = await new EmbedBuilder()
+        .setTitle('Region Creation')
+        .setThumbnail(
+          'attachment://tick-check-mark-icon-sign-symbol-design-free-png.webp'
+        )
+        .setColor(0x00ff7d)
+        .addFields([
+          {
+            name: 'Name',
+            value: region.name,
+            inline: false,
+          },
+          {
+            name: 'Role',
+            value: `<@&${regionRole.id}>`,
+            inline: false,
+          },
+          {
+            name: 'By',
+            value: `<@${interaction.user.id}>`,
+            inline: true,
+          },
+          {
+            name: '\u200b',
+            value: `<t:${Math.floor(Date.now() / 1000)}:R>`,
+            inline: true,
+          },
+        ])
+        .setTimestamp();
+
+      await interaction.guild.channels.cache
+        .find((channel) => channel.id === '1178199804753481789')
+        .send({
+          embeds: [regionEmbed],
+          files: [file],
+        });
     }
   },
 };
